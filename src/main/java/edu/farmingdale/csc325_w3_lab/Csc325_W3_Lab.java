d3be9fa84beac4a7b74c199ba48fc51afccbaeef
rinku <sarkr72@mail.sunysuffolk.edu>
2022-09-14 09:37:50 -0400

    hello world 2

---
+++ src/main/java/edu/farmingdale/csc325_w3_lab/Csc325_W3_Lab.java
@@ -0,0 +1,12 @@
+package edu.farmingdale.csc325_w3_lab;
+
+/**
+ *
+ * @author bijoy
+ */
+public class Csc325_W3_Lab {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
