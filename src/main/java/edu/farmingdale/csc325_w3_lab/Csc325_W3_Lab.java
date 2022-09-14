54d3eb50c9e16a7e0680ec60a4b10cfbe6f02030
rinku <sarkr72@mail.sunysuffolk.edu>
2022-09-14 10:17:09 -0400

    made instance2

--- src/main/java/edu/farmingdale/csc325_w3_lab/Csc325_W3_Lab.java
+++ src/main/java/edu/farmingdale/csc325_w3_lab/Csc325_W3_Lab.java
@@ -9,5 +9,7 @@ public class Csc325_W3_Lab {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World again!");
+        Student std2 = new Student();
+        System.out.println("student2");
     }
 }
