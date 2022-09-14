8b82f4b73e34891552d3dd6d81288484fc0c35f2
rinku <sarkr72@mail.sunysuffolk.edu>
2022-09-14 10:03:22 -0400

    added getter

--- src/main/java/edu/farmingdale/csc325_w3_lab/Student.java
+++ src/main/java/edu/farmingdale/csc325_w3_lab/Student.java
@@ -10,5 +10,17 @@ public class Student {
     String fName;
     String lName;
     double gpa;
-    
+
+    public String getfName() {
+        return fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+       
 }
